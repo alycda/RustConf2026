a3a94b1a00ef2bd249f1eca09b61c68380fe7df6
Alyssa Evans <alyda@me.com>
2026-08-24 01:18:10 +0000

    fix(devcontainer): mark the workspace itself as safe.directory, not its parent

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Mark repo as safe, see: https://code.visualstudio.com/docs/sourcecontrol/faq#_why-is-vs-code-warning-me-that-the-git-repository-is-potentially-unsafe (because of nix)
-git config --global --add safe.directory $(dirname "$PWD")
+git config --global --add safe.directory "$PWD"
